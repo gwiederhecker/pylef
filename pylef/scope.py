22a6a561a25e67946c97fa2a28cdbe325b176fe2
felippebarbosa <felippe.barbosa@gmail.com>
2017-03-06 16:15:13 -0300

    Update scope.py

--- pylef/scope.py
+++ pylef/scope.py
@@ -1,9 +1,10 @@
+-*- coding: utf-8 -*-
 ################################
 ##
 ##  Class for the Tektronics 
 ##  Osciloscopes
 ##
-##
+## @author: Felippe Barbosa
 #################################
 import visa   # interface with NI-Visa
 import numpy  # module for array manipulation
