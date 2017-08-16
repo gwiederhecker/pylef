31439ce269d4fd82b27eb5976c4d933d8fe85d3c
gwiederhecker <gsw@wifi-177-220-88-35.ifi.unicamp.br>
2017-08-16 10:06:00 -0300

    added methods functions
    not a class yet

---
+++ pylef/methods.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+
+""" Class for the Methods """
+
+#################################
+import visa   # interface with NI-Visa
+import numpy  # module for array manipulation
+import os   # module for general OS manipulation
+import time # module for time related funtions
+import pandas # module for general data analysis
+################################
+
+
+def 
+
+
